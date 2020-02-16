a8fe3c443cb0a23fdeff8b0a8595d4a02046c007
bengquist <engquist_blake@yahoo.com>
2020-02-15 19:16:34 -0600

    removed tailwind

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="text-purple-600">
+      <p>Home</p>
+      <p>Yo</p>
+    </div>
+  );
 }
 
 export default Home;
