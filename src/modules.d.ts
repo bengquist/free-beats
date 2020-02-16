4907d91d4b6b304e893712446bcbd8574a6946af
bengquist <engquist_blake@yahoo.com>
2020-02-15 23:37:18 -0600

    added ts modules for non JS files

---
+++ src/modules.d.ts
@@ -0,0 +1,35 @@
+// for non-JS asset imports
+declare module "*.png" {
+  const url: string;
+  export default url;
+}
+
+declare module "*.jpg" {
+  const url: string;
+  export default url;
+}
+
+declare module "*.gif" {
+  const url: string;
+  export default url;
+}
+
+declare module "*.svg" {
+  const url: string;
+  export default url;
+}
+
+declare module "*.mp4" {
+  const url: string;
+  export default url;
+}
+
+declare module "*.pdf" {
+  const url: string;
+  export default url;
+}
+
+declare module "*.zip" {
+  const url: string;
+  export default url;
+}
