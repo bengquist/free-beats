3d43051ae0103cfc64c1a3aff7e324928458596f
bengquist <engquist_blake@yahoo.com>
2020-02-22 16:31:18 -0600

    added context type for modal context

--- src/Modal/ModalContext.tsx
+++ src/Modal/ModalContext.tsx
@@ -1,7 +1,12 @@
 import React, { createContext, ReactNode, useState } from "react"
 import Portal from "./Portal"
 
-export const ModalContext = createContext({})
+type ContextProps = {
+  open: (node: ReactNode) => void
+  close: () => void
+}
+
+export const ModalContext = createContext<Partial<ContextProps>>({})
 
 function ModalProvider({ children }: { children: ReactNode }) {
   const [modal, setModal] = useState()
@@ -10,8 +15,12 @@ function ModalProvider({ children }: { children: ReactNode }) {
     setModal(node)
   }
 
+  const close = () => {
+    setModal(undefined)
+  }
+
   return (
-    <ModalContext.Provider value={{ open }}>
+    <ModalContext.Provider value={{ open, close }}>
       {modal && <Portal>{modal}</Portal>}
       {children}
     </ModalContext.Provider>
