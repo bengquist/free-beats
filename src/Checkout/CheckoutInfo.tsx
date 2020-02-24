d7202832c05a040e7f5b00f8421421ae4ba5383d
bengquist <engquist_blake@yahoo.com>
2020-02-23 20:21:46 -0600

    added env variables to front end

--- src/Checkout/CheckoutInfo.tsx
+++ src/Checkout/CheckoutInfo.tsx
@@ -5,8 +5,6 @@ import styled from "styled-components"
 import { useCartContext } from "../Cart/CartContext"
 import CheckoutForm from "./CheckoutForm"
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
 const stripePromise = loadStripe(process.env.STRIPE_TEST_PUB_KEY)
 
 function CheckoutInfo() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,67 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const webpack = require("webpack")
+const dotenv = require("dotenv")
 
-module.exports = {
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  entry: path.resolve(__dirname, "src/index.tsx"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts)x?$/,
-        include: path.resolve(__dirname, "src"),
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.(js|ts)x?$/,
-        enforce: "pre",
-        use: [
-          {
-            options: {
-              eslintPath: require.resolve("eslint"),
+module.exports = () => {
+  // call dotenv and it will return an Object with a parsed key
+  const env = dotenv.config().parsed
+
+  // reduce it to a nice object, the same as before
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next])
+    return prev
+  }, {})
+
+  return {
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    entry: path.resolve(__dirname, "src/index.tsx"),
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts)x?$/,
+          include: path.resolve(__dirname, "src"),
+          use: ["babel-loader"],
+        },
+        {
+          test: /\.(js|ts)x?$/,
+          enforce: "pre",
+          use: [
+            {
+              options: {
+                eslintPath: require.resolve("eslint"),
+              },
+              loader: require.resolve("eslint-loader"),
             },
-            loader: require.resolve("eslint-loader"),
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpg|gif|svg|ttf|woff|eot|mp3|mp4|zip|pdf)$/i,
-        use: [
-          {
-            loader: "url-loader",
-            options: { name: "static/[hash:16].[ext]", limit: 8192 },
-          },
-        ],
-      },
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif|svg|ttf|woff|eot|mp3|mp4|zip|pdf)$/i,
+          use: [
+            {
+              loader: "url-loader",
+              options: { name: "static/[hash:16].[ext]", limit: 8192 },
+            },
+          ],
+        },
+      ],
+    },
+    devServer: {
+      historyApiFallback: true,
+      contentBase: path.resolve(__dirname, "dist"),
+      port: 9000,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "public/index.html",
+      }),
+      new webpack.DefinePlugin(envKeys),
     ],
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, "dist"),
-    port: 9000,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "public/index.html", //source html
-    }),
-  ],
+  }
 }
